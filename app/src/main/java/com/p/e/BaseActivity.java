12cdfe30b6138953e91da2b30b79469ec24becab
takieddine12 <algeriahero22@gmail.com>
2024-03-13 20:14:37 +0100

    new changes

---
+++ app/src/main/java/com/p/e/BaseActivity.java
@@ -0,0 +1,81 @@
+package com.p.e;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private static final int[] ORDERED_DENSITY_DP = {
+            DisplayMetrics.DENSITY_LOW,
+            DisplayMetrics.DENSITY_MEDIUM,
+            DisplayMetrics.DENSITY_TV,
+            DisplayMetrics.DENSITY_HIGH,
+            DisplayMetrics.DENSITY_260,
+            DisplayMetrics.DENSITY_280,
+            DisplayMetrics.DENSITY_XHIGH,
+            DisplayMetrics.DENSITY_300,
+            DisplayMetrics.DENSITY_600,
+            DisplayMetrics.DENSITY_520,
+            DisplayMetrics.DENSITY_450,
+            DisplayMetrics.DENSITY_340,
+            DisplayMetrics.DENSITY_360,
+            DisplayMetrics.DENSITY_400,
+            DisplayMetrics.DENSITY_420,
+            DisplayMetrics.DENSITY_440,
+            DisplayMetrics.DENSITY_XXHIGH,
+            DisplayMetrics.DENSITY_560,
+            DisplayMetrics.DENSITY_XXXHIGH,
+    };
+
+
+    @Override
+    protected void attachBaseContext(final Context baseContext) {
+
+        Context newContext = baseContext;
+
+        // Screen zoom is supported from API 24+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            Resources resources = baseContext.getResources();
+            DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+            Configuration configuration = resources.getConfiguration();
+
+
+            if (displayMetrics.densityDpi != DisplayMetrics.DENSITY_DEVICE_STABLE) {
+                if(Settings.Global.getString(baseContext.getContentResolver(), "display_size_forced") != null) {
+                    float defaultDensity = (DisplayMetrics.DENSITY_DEVICE_STABLE / (float) DisplayMetrics.DENSITY_DEFAULT);
+                    float defaultScreenWidthDp = displayMetrics.widthPixels / defaultDensity;
+                    configuration.densityDpi = findDensityDpCanFitScreen((int) defaultScreenWidthDp);
+                } else {
+                    configuration.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE;
+                }
+                configuration.fontScale = 1.0f;
+                newContext = baseContext.createConfigurationContext(configuration);
+            }
+
+        }
+        super.attachBaseContext(newContext);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private static int findDensityDpCanFitScreen(final int densityDp) {
+        int[] orderedDensityDp = ORDERED_DENSITY_DP;
+        int index = 0;
+        while (densityDp >= orderedDensityDp[index]) {
+            index++;
+        }
+        return orderedDensityDp[index];
+    }
+}
+
---
+++ app/src/main/java/com/p/e/MainActivity.java
@@ -0,0 +1,47 @@
+package com.p.e;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Show the dialog when needed
+        showCustomAlertDialog("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+    }
+
+
+    private void showCustomAlertDialog(String message) {
+        // Create an AlertDialog builder
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        // Get the LayoutInflater
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // Inflate the custom layout
+        View dialogView = inflater.inflate(R.layout.custom_alert_dialog, null);
+
+        // Get the TextView in the custom layout
+        TextView dialogText = dialogView.findViewById(R.id.dialog_text);
+
+        // Set the message
+        dialogText.setText(message);
+
+        // Set the inflated view to the builder
+        dialogBuilder.setView(dialogView);
+
+        // Create and show the dialog
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+}
